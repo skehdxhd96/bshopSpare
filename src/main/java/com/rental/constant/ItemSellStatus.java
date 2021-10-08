6324cc58aaf899aaf1e037f45322c85246f5da48
EighthJ <chanbin7172@naver.com>
2021-10-08 18:25:58 +0900

    first commit

---
+++ src/main/java/com/rental/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.rental.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/rental/entity/Item.java
@@ -0,0 +1,40 @@
+package com.rental.entity;
+
+import com.rental.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    @Id
+    @Column(name = "item_id")
+    private Long id;       //상품 코드
+
+    @Column(nullable = false, length = 50)
+    private String itemNm; //상품명
+
+    @Column(name = "price", nullable = false)
+    private int price; //가격
+
+    @Column(nullable = false)
+    private int stockNumber; //재고수량
+
+    @Column(nullable = false)
+    private String itemDetail; //상품 상세 설명
+    @Enumerated ()
+    private ItemSellStatus itemSellStatus; //상품 판매 상태
+
+    private LocalDateTime regtime; //상품 등록 시간
+
+    private LocalDateTime updatetime; //수정 시간
+
+}
